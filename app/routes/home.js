5c0f2dd95467cb52cd7dfb3854952a183afbd43b
Luis Hlatki <luis@dotsolucoes.com.br>
2020-06-24 11:28:20 -0300

    commit

--- app/routes/home.js
+++ app/routes/home.js
@@ -1,15 +1,15 @@
 module.exports = function(application){
 
 	application.get('/login', function(req, res){
-		application.app.controllers.home.index(application, req, res);
+		application.app.app.controllers.home.index(application, req, res);
 	});
 
 	application.get('/', function(req, res){
-		application.app.controllers.home.index(application, req, res);
+		application.app.app.controllers.home.index(application, req, res);
 	});
 
 	application.post('/autenticar', function(req, res){
-		application.app.controllers.home.autenticar(application, req, res);
+		application.app.app.controllers.home.autenticar(application, req, res);
 	});
 
 	application.get('/sair', function(req, res){
